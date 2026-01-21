6ffecd69785777881271b3a1b603afb8a47639a1
kholodost <kholod.ost@mail.ru>
2026-01-21 19:14:34 +0300

    repair slider, add new section about doctors

--- main.js
+++ main.js
@@ -2,10 +2,26 @@ $(document).ready(function(){
   $('.openpage-slider').slick({
     arrows: false,
     adaptiveHeight: true,
-    draggable: false,
-    // dots: true,
-    // appendDots: $('.slider-dots'),
+    draggable: true,
+    dots: true,
+    appendDots: $('.slider-dots'),
+    customPaging: function() {
+    return '<button type="button"></button>';
+  }
   });
 });
 
+
+$(document).ready(function(){
+  $('.selector-slider-doctors').slick({
+    arrows: false,
+    adaptiveHeight: true,
+    draggable: true,
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    rows: 2
+    // appendDots: $('.slider-dots'),
+  });
+});
 // хз как запустить и настроить другого вида dots
