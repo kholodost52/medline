a72bcde98c212ca8ef3235fa2cc6ef80e4fa5015
kholodost <kholod.ost@mail.ru>
2026-01-22 16:54:48 +0300

    fix slider with doctors

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
-$(document).ready(function(){
-  $('.openpage-slider').slick({
+$(document).ready(function () {
+  $(".openpage-slider").slick({
     arrows: false,
     adaptiveHeight: true,
     draggable: true,
     dots: true,
-    appendDots: $('.slider-dots'),
-    customPaging: function() {
-    return '<button type="button"></button>';
-  }
+    appendDots: $(".slider-dots"),
+    customPaging: function () {
+      return '<button type="button"></button>';
+    },
   });
 });
 
-
-$(document).ready(function(){
-  $('.selector-slider-doctors').slick({
+$(document).ready(function () {
+  $(".selector-slider-doctors").slick({
     arrows: false,
     adaptiveHeight: true,
     draggable: true,
     dots: true,
     infinite: true,
     slidesToShow: 3,
-    rows: 2
-    // appendDots: $('.slider-dots'),
+    rows: 2,
+    appendDots: $('.dots-doctors-slider'),
+    customPaging: function () {
+      return '<button type="button"></button>';
+    },
   });
 });
 // хз как запустить и настроить другого вида dots
