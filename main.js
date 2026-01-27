ee3eab688c608847e4df1743b50239ccc1c2d4c8
kholodost <kholod.ost@mail.ru>
2026-01-27 15:07:44 +0300

    add map-medline

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function () {
   $(".openpage-slider").slick({
     arrows: false,
@@ -26,3 +27,33 @@ $(document).ready(function () {
   });
 });
 // хз как запустить и настроить другого вида dots
+
+
+initMap();
+
+async function initMap() {
+    // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
+    await ymaps3.ready;
+
+    const {YMap, YMapDefaultSchemeLayer} = ymaps3;
+
+    // Иницилиазируем карту
+    const map = new YMap(
+        // Передаём ссылку на HTMLElement контейнера
+        document.getElementById('map'),
+
+        // Передаём параметры инициализации карты
+        {
+            location: {
+                // Координаты центра карты
+                center: [37.588144, 55.733842],
+
+                // Уровень масштабирования
+                zoom: 10
+            }
+        }
+    );
+
+    // Добавляем слой для отображения схематической карты
+    map.addChild(new YMapDefaultSchemeLayer());
+}
